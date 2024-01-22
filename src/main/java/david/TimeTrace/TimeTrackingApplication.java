c2f7bd107efc06264ddcac339b5c2b3a758581e7
DoubleDRG <dyl660115@gmail.com>
2024-01-22 13:28:23 +0900

    [feat] View 구현

---
+++ src/main/java/david/TimeTrace/TimeTrackingApplication.java
@@ -0,0 +1,15 @@
+package david.TimeTrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackingApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TimeTrackingApplication.class, args);
+    }
+
+}
