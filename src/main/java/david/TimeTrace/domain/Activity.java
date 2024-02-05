cb78c2e69d1c9fdc06971e775bc82fecb88b62ea
DoubleDRG <dyl660115@gmail.com>
2024-02-05 21:28:57 +0900

    [refactor] Activity 도메인 수정

--- src/main/java/david/TimeTrace/domain/Activity.java
+++ src/main/java/david/TimeTrace/domain/Activity.java
@@ -1,21 +1,24 @@
 package david.TimeTrace.domain;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.stereotype.Controller;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-@Entity
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
 @Getter
 @Setter
+@NoArgsConstructor(access = PROTECTED)
+@Entity
 public class Activity
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "activity_id")
     private Long id;
     private String title;
 
@@ -29,10 +32,6 @@ public class Activity
 
     @Column(length = 20000)
     private String content;
-
-    protected Activity()
-    {
-    }
 
     @Builder
     public Activity(String title, String stacks, LocalDateTime startTime, LocalDateTime endTime, Long duration, String content)
